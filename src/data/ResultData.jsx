6186b7aa8ebd15ae90fa87ef9dfa83b744a671a8
leesh9098 <leesh9098@naver.com>
2022-02-11 12:03:37 +0900

    shart test

--- src/data/ResultData.jsx
+++ src/data/ResultData.jsx
@@ -1,4 +1,3 @@
-import ResultThumbnail from "../images/resultthumbnail.png";
 import Level1 from "../images/result_level1.png";
 import Level2 from "../images/result_level2.png";
 import Level3 from "../images/result_level3.png";
@@ -10,28 +9,28 @@ const ResultData = {
         src: Level1,
         ogTitle: "아직은 귀여운 새내기",
         ogDescription: "LEVEL 1",
-        ogImage: ResultThumbnail
+        ogImage: Level1
     },
     level2: {
         title: 'LEVEL 2',
         src: Level2,
         ogTitle: "도서관에 기숙하는 헌내기",
         ogDescription: "LEVEL 2",
-        ogImage: ResultThumbnail
+        ogImage: Level2
     },
     level3: {
         title: 'LEVEL 3',
         src: Level3,
         ogTitle: "츄리닝만 입고다니는 고인물",
         ogDescription: "LEVEL 3",
-        ogImage: ResultThumbnail
+        ogImage: Level3
     },
     level4: {
         title: 'LEVEL 4',
         src: Level4,
         ogTitle: "고인물이 굳어 만들어진 화석",
         ogDescription: "LEVEL 4",
-        ogImage: ResultThumbnail
+        ogImage: Level4
     },
 }
 
--- src/stage/Prolog.jsx
+++ src/stage/Prolog.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import ReactGA from "react-ga4";
 import "../reset.css";
 import "../App.css";
+import { Helmet } from "react-helmet";
+import MainThumbnail from "../images/mainthumbnail.png";
 
 export default class Prolog extends React.Component {
 
@@ -13,20 +15,32 @@ export default class Prolog extends React.Component {
 
     render() {
         return (
-            <div className="wrap">
-                <div className="titlebox">
-                    <p className="museumBold titletext">2022</p>
-                    <p className="museumBold titletext">대학생 능력고사</p>
+            <>
+                <Helmet>
+                    <meta property="og:url" content={window.location.href} />
+                    <meta property="og:title" content="2022 대학생 능력고사" />
+                    <meta property="og:description" content="당신의 대학교 용어 점수는?" />
+                    <meta property="og:image" content={MainThumbnail} />
+                    <meta property="twitter:card" content="summary" />
+                    <meta name="twitter:title" content="2022 대학생 능력고사" />
+                    <meta name="twitter:description" content="당신의 대학교 용어 점수는?" />
+                    <meta name="twitter:image" content={MainThumbnail} />
+                </Helmet>
+                <div className="wrap">
+                    <div className="titlebox">
+                        <p className="museumBold titletext">2022</p>
+                        <p className="museumBold titletext">대학생 능력고사</p>
+                    </div>
+                    <p className="museumMedium subtitletext">당신의 대학교 용어 점수는 과연?</p>
+                    <div className="line"></div>
+                    <div className="imagearea"></div>
+                    <p className="suitRegular numberofuser">현재 10,402명이 참여</p>
+                    <Link to="/quiz">
+                        <button className="nextbutton">시작하기</button>
+                    </Link>
+                    <div className="logoarea"></div>
                 </div>
-                <p className="museumMedium subtitletext">당신의 대학교 용어 점수는 과연?</p>
-                <div className="line"></div>
-                <div className="imagearea"></div>
-                <p className="suitRegular numberofuser">현재 10,402명이 참여</p>
-                <Link to="/quiz">
-                    <button className="nextbutton">시작하기</button>
-                </Link>
-                <div className="logoarea"></div>
-            </div>
+            </>
         )
     }
 }
