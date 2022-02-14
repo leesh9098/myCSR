a60217d2769ca6c96629b48b66ae83edd919ac7a
leesh9098 <leesh9098@naver.com>
2022-02-14 10:54:28 +0900

    22.02.14 최종 점수 추가

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -4,6 +4,7 @@ import ResultData from "../data/ResultData";
 const ResultSection = ({ param }) => {
     return (
         <>
+            <p style={{ color: 'red', fontWeight: 'bold', fontSize: "30px" }}>{window.atob(window.sessionStorage.getItem("JUVEJTk4JTg0JUVDJTlFJUFDJUVDJUEwJTkwJUVDJTg4JTk4"))}</p>
             <img
                 src={ResultData[param].src}
                 alt="결과"
